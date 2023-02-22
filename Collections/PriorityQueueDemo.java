5c904699e651b7883c03cbe7fabac1581cc39e1b
MSreeN <khyathimnsree369@gmail.com>
2023-02-22 13:11:31 +0530

    PriorityDeque

---
+++ Collections/PriorityQueueDemo.java
@@ -0,0 +1,24 @@
+package Collections;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class MyComp implements Comparator<Integer>{
+  public int compare(Integer a , Integer b){
+    if(a<b) return 1;
+    if(a>b) return -1;
+    return 0;
+  }
+}
+
+public class PriorityQueueDemo {
+  public static void main(String[] args) {
+    PriorityQueue<Integer> pq = new PriorityQueue<>(new MyComp());
+    pq.add(5);
+    pq.add(8);
+    pq.add(1);
+    pq.add(2);
+    pq.remove();
+    pq.forEach((ele) -> System.out.println(ele));
+  }
+}
