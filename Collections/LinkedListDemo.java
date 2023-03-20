06dfa4890f3a5638a161175f2eff9e962746dbc6
MSreeN <khyathimnsree369@gmail.com>
2023-03-20 11:28:10 +0530

    LinkedListDemo.java

--- Collections/LinkedListDemo.java
+++ Collections/LinkedListDemo.java
@@ -12,21 +12,23 @@ public class LinkedListDemo {
     placesToVisit.add(0, "bza");
     placesToVisit.addFirst("bangalore");
     placesToVisit.addAll(new ArrayList<String>(List.of("hello")));
-    String first  = placesToVisit.element();
-    System.out.println(first);
+    // String first  = placesToVisit.element();
+    // System.out.println(first);
     ListIterator<String> li = placesToVisit.listIterator();
     while(li.hasNext()){
-      System.out.println(li.next());
+      // System.out.println(li.next());
     }
     placesToVisit.push("nuz");
     placesToVisit.add("elr");
     placesToVisit.offer("last");
-    System.out.println(placesToVisit);
+    // System.out.println(placesToVisit);
     placesToVisit.remove();
-    System.out.println(placesToVisit.isEmpty());
+    // System.out.println(placesToVisit.isEmpty());
     int ind = placesToVisit.indexOf("hello");
     System.out.println(ind);
-    System.out.println(placesToVisit.contains("bangalore"));
-    System.out.println(placesToVisit);
+    // System.out.println(placesToVisit.contains("bangalore"));
+    // System.out.println(placesToVisit);
+
+    // list iterator
   }
 }
