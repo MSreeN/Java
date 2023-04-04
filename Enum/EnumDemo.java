d10c482a427e252a4a1bcc14a19b748b248b399e
MSreeN <khyathimnsree369@gmail.com>
2023-04-04 19:41:18 +0530

    enum

--- Enum/EnumDemo.java
+++ Enum/EnumDemo.java
@@ -6,13 +6,16 @@ import java.util.Random;
 public class EnumDemo {
   public static void main(String[] args) {
     DayOfTheWeek dow = DayOfTheWeek.SUN;
-    // System.out.println(dow);
+
+    System.out.println(dow.name());
     DayOfTheWeek[] val = DayOfTheWeek.values();
     Random random = new Random();
     for(int i = 0; i<5; i++){
       int randNum = random.nextInt(7);
         switch(randNum){
-          case 0 : System.out.println("monday");
+          case 0 -> System.out.println("monday");
+          case 1 -> System.out.println("tues");
+          
         }
       }
     }
