f5e51a81783133ed759d9a0e927d60ae0bc1ed50
MSreeN <khyathimnsree369@gmail.com>
2023-04-16 16:52:18 +0530

    added seat file

--- Collections/Main.java
+++ Collections/Main.java
@@ -1,18 +1,26 @@
 package Collections;
 
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
     Theatre theatre = new Theatre("Olympian", 8, 12);
-    theatre.getSeats();
-    theatre.reserveSeat("H08");
-    theatre.reserveSeat("H08");
-    theatre.reserveSeat("A01");
-    theatre.reserveSeat("A01");
-    theatre.getTheatreName();
-    theatre.reserveSeat("D04");
-    theatre.reserveSeat("D04");
-    theatre.reserveSeat("B05");
-    theatre.reserveSeat("B07");
-    theatre.reserveSeat("B05");
+    // theatre.getSeats();
+    // theatre.reserveSeat("H08");
+    // theatre.reserveSeat("H08");
+    // theatre.reserveSeat("A01");
+    // theatre.reserveSeat("A01");
+    // theatre.getTheatreName();
+    // theatre.reserveSeat("D04");
+    // theatre.reserveSeat("D04");
+    // theatre.reserveSeat("B05");
+    // theatre.reserveSeat("B07");
+    // theatre.reserveSeat("B05");
+  }
+
+  public static void printList(List<Theatre.Seat> list){
+    for(Theatre.Seat seat : list){
+      System.out.println(seat);
+    }
   }
 }
