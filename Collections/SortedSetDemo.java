0c67979a9b26de1b9ba12a13216b6b09e9a99d81
MSreeN <khyathimnsree369@gmail.com>
2023-04-23 10:03:02 +0530

    sorted set

---
+++ Collections/SortedSetDemo.java
@@ -0,0 +1,14 @@
+package Collections;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class SortedSetDemo {
+  public static void main(String[] args) {
+    SortedSet s = new TreeSet<>();
+    s.add(5);
+    s.add(6);
+    s.add(3);
+    System.out.println(s.first());
+  }
+}
