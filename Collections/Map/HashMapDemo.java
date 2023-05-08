1a209f61ffdeaa176bf62c159abbf1d157913c3c
MSreeN <khyathimnsree369@gmail.com>
2023-05-08 11:05:59 +0530

    HashMap

--- Collections/Map/HashMapDemo.java
+++ Collections/Map/HashMapDemo.java
@@ -8,6 +8,12 @@ public class HashMapDemo {
     Map<String, Integer> map = new HashMap();
     map.put("Bio", 99);
     map.put("English", 90);
+    //Can have null as key
+    map.put(null, 50);
+    map.put(null, 55);
+    map.computeIfAbsent("sree", v -> 5);
+    //If the key is repeated then it replaces the value of that corresponding key with new value
+    map.put("Bio", 100);
     System.out.println(map);
   }
 }
