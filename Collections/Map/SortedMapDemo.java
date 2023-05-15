65cc8b449370117a95612e0c9df04a9152d3a85f
MSreeN <khyathimnsree369@gmail.com>
2023-05-15 11:19:22 +0530

    Sorted Map

--- Collections/Map/SortedMapDemo.java
+++ Collections/Map/SortedMapDemo.java
@@ -6,10 +6,14 @@ import java.util.TreeMap;
 
 public class SortedMapDemo {
   public static void main(String[] args) {
-    SortedMap sortedMap = new TreeMap<>(Comparator.reverseOrder());
+    SortedMap sortedMap = new TreeMap<>();
     sortedMap.put(3, "Hello");
     sortedMap.put(5, "world");
     sortedMap.put(1,"welcome");
     System.out.println(sortedMap);
+    System.out.println(sortedMap.tailMap(5));
+    System.out.println(sortedMap.keySet());
+    System.out.println(sortedMap.values());
+    System.out.println(sortedMap.entrySet());
   }
 }
