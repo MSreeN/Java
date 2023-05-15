2e6a437cdb56b051c5193471c1bc5581d741b71e
MSreeN <khyathimnsree369@gmail.com>
2023-05-15 11:23:42 +0530

    Idenity hash map

--- Collections/Map/IdentityHashMapDemo.java
+++ Collections/Map/IdentityHashMapDemo.java
@@ -7,6 +7,9 @@ public class IdentityHashMapDemo {
     IdentityHashMap identityHashMap = new IdentityHashMap<>();
     identityHashMap.put("a", 1);
     identityHashMap.put(new String("a"), 2);
+    //Since single reference will point to these two 10's in the literal pool, Identity hash map won't allow for duplicate keys.
+    identityHashMap.put(10,"one");
+    identityHashMap.put(10, "two");
     System.out.println(identityHashMap);
   }
 }
