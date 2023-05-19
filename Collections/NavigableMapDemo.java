2950975d074c562c7e43ab80d54326d6b50b3ec2
MSreeN <khyathimnsree369@gmail.com>
2023-05-19 22:31:07 +0530

    navigalbe map

--- Collections/NavigableMapDemo.java
+++ Collections/NavigableMapDemo.java
@@ -21,7 +21,9 @@ public class NavigableMapDemo {
     System.out.println("descending key"+ navigableMap.descendingMap());
     System.out.println("head map "+ navigableMap.headMap(4, true));
     System.out.println("subMap "+ navigableMap.subMap(2, 4));
+    //returns the entries form the specified key, inclusion of the entry of the specified key depends on the boolean value
     System.out.println("tail map "+ navigableMap.tailMap(2 ,false));
+    //returns the  next key that is higher that the specified key
     System.out.println("Higher "+ navigableMap.higherKey(4));
   }
 }
