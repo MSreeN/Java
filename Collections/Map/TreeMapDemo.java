fc18bab341de736ef39c0244b7211b2b3216acec
MSreeN <khyathimnsree369@gmail.com>
2023-05-23 21:14:29 +0530

    treemap

--- Collections/Map/TreeMapDemo.java
+++ Collections/Map/TreeMapDemo.java
@@ -1,5 +1,14 @@
 package Collections.Map;
 
+import java.util.Comparator;
+import java.util.TreeMap;
+
 public class TreeMapDemo {
-  
+  public static void main(String[] args) {
+    TreeMap treeMap = new TreeMap<>(Comparator.reverseOrder());
+    treeMap.put(5, "hello");
+    treeMap.put(1, "world");
+    treeMap.put(3, "wel");
+    System.out.println(treeMap);
+  }
 }
