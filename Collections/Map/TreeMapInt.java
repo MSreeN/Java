663bd1d62762a3922fe2c361fceca67f0f3071f1
MSreeN <khyathimnsree369@gmail.com>
2023-05-24 20:57:46 +0530

    tree map sorting using comparable

--- Collections/Map/TreeMapInt.java
+++ Collections/Map/TreeMapInt.java
@@ -2,8 +2,13 @@ package Collections.Map;
 
 public class TreeMapInt implements Comparable {
   int a;
-
+  TreeMapInt(int a){
+    this.a =a ;
+  }
   public int compareTo(Object object){
-
+    Integer s1 = (Integer)object;
+    if(this.a > s1) return -1;
+    if(this.a < s1) return 1;
+    else return 0;
   }
 }
