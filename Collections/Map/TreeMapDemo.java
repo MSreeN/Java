58f3492007941918ac9cfe6b1205cd838b5b9f6d
MSreeN <khyathimnsree369@gmail.com>
2023-05-24 20:54:01 +0530

    treemap sorting

--- Collections/Map/TreeMapDemo.java
+++ Collections/Map/TreeMapDemo.java
@@ -6,10 +6,10 @@ import java.util.TreeMap;
 public class TreeMapDemo {
   public static void main(String[] args) {
     TreeMap treeMap = new TreeMap<>();
-    treeMap.put("d", 1);
-    treeMap.put("a", 5);
-    treeMap.put("z",8);
-    treeMap.put("k",1);
+    // treeMap.put("d", 1);
+    // treeMap.put("a", 5);
+    // treeMap.put("z",8);
+    // treeMap.put("k",1);
     System.out.println(treeMap);
   }
 }
---
+++ Collections/Map/TreeMapInt.java
@@ -0,0 +1,9 @@
+package Collections.Map;
+
+public class TreeMapInt implements Comparable {
+  int a;
+
+  public int compareTo(Object object){
+
+  }
+}
