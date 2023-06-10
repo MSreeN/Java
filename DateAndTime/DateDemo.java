9a7425f49d8d466aac353ccf9a05a03bfe1692b9
MSreeN <khyathimnsree369@gmail.com>
2023-06-10 10:08:41 +0530

    gregorian calendar

--- DateAndTime/DateDemo.java
+++ DateAndTime/DateDemo.java
@@ -1,11 +1,17 @@
 package DateAndTime;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DateDemo {
   public static void main(String[] args) {
     System.out.println(System.currentTimeMillis()/1000/60/60/24/365);
     Date date = new Date();
     System.out.println(date);
+    GregorianCalendar gc =  new GregorianCalendar();
+    System.out.println(gc.isLeapYear(2024));
+    System.out.println(date.getDay());
+    System.out.println(gc.get(Calendar.DAY_OF_WEEK));
   }
 }
