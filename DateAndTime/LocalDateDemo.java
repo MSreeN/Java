a20ea8bf672ac4e0dcfd6b9f6b308e946d3fbd96
MSreeN <khyathimnsree369@gmail.com>
2023-06-27 21:13:32 +0530

    Localdate class

--- DateAndTime/LocalDateDemo.java
+++ DateAndTime/LocalDateDemo.java
@@ -1,10 +1,14 @@
 package DateAndTime;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.ZoneId;
 
 public class LocalDateDemo {
   public static void main(String[] args) {
     LocalDate ld = LocalDate.now();
     System.out.println(ld); 
+    LocalDate ld1 = LocalDate.of(2001,Month.OCTOBER, 25);
+    System.out.println(ld1);
   }
 }
