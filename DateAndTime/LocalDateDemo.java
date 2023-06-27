560b76cd8a56cb3a091966f0455036b0b0e5da9b
MSreeN <khyathimnsree369@gmail.com>
2023-06-27 21:32:42 +0530

    LocalDate

--- DateAndTime/LocalDateDemo.java
+++ DateAndTime/LocalDateDemo.java
@@ -9,6 +9,11 @@ public class LocalDateDemo {
     LocalDate ld = LocalDate.now();
     System.out.println(ld); 
     LocalDate ld1 = LocalDate.of(2001,Month.OCTOBER, 25);
+    ld1 = LocalDate.ofEpochDay(5);
     System.out.println(ld1);
+    System.out.println(ld1.getDayOfMonth());
+    LocalDate cp = ld1.plusDays(10);
+
+    System.out.println(cp);
   }
 }
