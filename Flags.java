5153dd35e6f1f1a1799ca7ea50edd41387698f63
MSreeN <khyathimnsree369@gmail.com>
2023-07-09 23:26:21 +0530

    jdbc implementation

--- Flags.java
+++
@@ -1,6 +0,0 @@
-public class Flags {
-    public static void m̥ain(String[] ̥args) {
-        int ̥az= 10, b = 20;
-        // System.out.printf("%-5d"̥);
-    }
-}
--- JDBC/Database.java
+++ JDBC/Database.java
@@ -1,7 +1,23 @@
 package JDBC;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Database {
-  public static void main(String[] args) {
-    
+  public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    Class.forName("org.sqlite.JDBC");
+    Connection con = DriverManager.getConnection("jdbc:sqlite:C://sqlite//univ.db");
+    java.sql.Statement statement = con.createStatement();
+    ResultSet resultSet = statement.executeQuery("select * from dept");
+
+    int deptNo;
+    String deptName;
+    while(resultSet.next()){
+      deptNo = resultSet.getInt("deptno");
+      deptName = resultSet.getString("dname");
+      System.out.println(deptNo+ "-----"+ deptName);
+    }
   }
 }
