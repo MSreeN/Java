818114173def3d258b8078719e969dd0948580d9
MSreeN <khyathimnsree369@gmail.com>
2023-07-10 22:49:42 +0530

    fetched data from database

--- JDBC/Database/src/App.java
+++ JDBC/Database/src/App.java
@@ -1,5 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Class.forName("org.sqlite.JDBC");
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:C://sqlite//univ.db");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from dept");
+        while (resultSet.next()) {
+            System.out.print(resultSet.getInt("deptNo") + " ");
+            System.out.println(resultSet.getString("dname"));
+        }
     }
 }
