ace166768abbcdcb63cdfbf0f349a7cc3b00ed35
MSreeN <khyathimnsree369@gmail.com>
2023-07-12 22:20:28 +0530

    creating table

--- JDBC/Database/src/App.java
+++ JDBC/Database/src/App.java
@@ -17,9 +17,21 @@ public class App {
         Class.forName("org.sqlite.JDBC");
         Connection connection = DriverManager.getConnection("jdbc:sqlite:C://sqlite//univ.db");
         Statement statement = connection.createStatement();
+
+        ///////////insertion////////////////
         // statement.executeUpdate("insert into dept values(50,'Petroleum')");
+
+        //////////////deletion from table///////////
         // statement.executeUpdate("delete from dept where deptno>50");
-        statement.executeUpdate("update dept set dname = 'Chem' where deptno = 50");
+
+        //////////updating//////////////
+
+        // statement.executeUpdate("update dept set dname = 'Chem' where deptno = 50");
+
+
+        //////////////creation of table///////////////
+
+        statement.executeUpdate("create table Temp(a integer, b integer)");
 
         //////////closing connections////////////////
 
