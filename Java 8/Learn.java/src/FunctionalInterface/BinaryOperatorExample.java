e168b2c4c381aef4780a3fc1484b2d2653356329
MSreeN <khyathimnsree369@gmail.com>
2023-08-17 21:21:28 +0530

    BinaryOperator

--- Java 8/Learn.java/src/FunctionalInterface/BinaryOperatorExample.java
+++ Java 8/Learn.java/src/FunctionalInterface/BinaryOperatorExample.java
@@ -7,6 +7,8 @@ public class BinaryOperatorExample {
     public static void main(String[] args) {
         Comparator<Integer> comparator = (a, b) -> a.compareTo(b);
         BinaryOperator<Integer> binaryOperator  = BinaryOperator.maxBy(comparator);
+        BinaryOperator<Integer> minBy = BinaryOperator.minBy(comparator);
+        System.out.println(minBy.apply(7,1));
         System.out.println(binaryOperator.apply(8,6));
     }
 }
