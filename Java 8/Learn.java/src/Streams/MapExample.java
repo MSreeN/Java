8bd2df892b67696d0e29a692e95439d41bfb839d
MSreeN <khyathimnsree369@gmail.com>
2023-08-24 23:14:46 +0530

    updated flatmap code

--- Java 8/Learn.java/src/Streams/MapExample.java
+++ Java 8/Learn.java/src/Streams/MapExample.java
@@ -8,10 +8,16 @@ import java.util.stream.Collectors;
 public class MapExample {
 
     public static void main(String[] args) {
-        System.out.println(getNamesList());
+        System.out.println(getActivities());
     }
 
     public static List<String> getNamesList(){
         return StudentDataBase.getAllStudents().stream().map(com.learnJava.data.Student::getName).collect(Collectors.toList());
     }
+
+    public static List<String> getActivities(){
+        return StudentDataBase.getAllStudents().stream().flatMap(s -> s.getActivities().stream())
+                .collect(Collectors.toList());
+    }
+
 }
