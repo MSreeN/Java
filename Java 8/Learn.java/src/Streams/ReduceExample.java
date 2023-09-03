1300e692c09bc3378d7b57e36be4bedac540e3cd
MSreeN <khyathimnsree369@gmail.com>
2023-09-03 21:15:23 +0530

    reduce method

--- Java 8/Learn.java/src/Streams/ReduceExample.java
+++ Java 8/Learn.java/src/Streams/ReduceExample.java
@@ -10,16 +10,23 @@ import java.util.Optional;
 public class ReduceExample {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1,3, 5, 7);
-//        System.out.println(multiplication(list));
+        List<Character> charList = Arrays.asList('a', 'b', 'C');
+//        System.out.println(multiplication(charList));
         System.out.println(topStudent());
     }
 
     public static Optional<com.learnJava.data.Student> topStudent(){
+//        return StudentDataBase.getAllStudents().stream()
+//                .reduce((s1,s2) -> s1.getGpa()>s2.getGpa() ? s1 : s2);
         return StudentDataBase.getAllStudents().stream()
-                .reduce((s1,s2) -> s1.getGpa()>s2.getGpa() ? s1 : s2);
+                .reduce((s1,s2) ->{
+                   if(s1.getGradeLevel() > s2.getGradeLevel()) return s1;
+                   else  return s2;
+                });
     }
 
     public static int multiplication(List<Integer> integerList){
         return integerList.stream().reduce(1, (acc, cVal) -> acc*cVal);
+
     }
 }
