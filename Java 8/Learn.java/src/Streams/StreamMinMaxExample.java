b63d4d14a73eb6d2760611f4ad562f29f62a4cfa
MSreeN <khyathimnsree369@gmail.com>
2023-09-11 22:22:24 +0530

    reduce without accumulator

--- Java 8/Learn.java/src/Streams/StreamMinMaxExample.java
+++ Java 8/Learn.java/src/Streams/StreamMinMaxExample.java
@@ -1,16 +1,30 @@
 package Streams;
 
-import java.util.Arrays;
-import java.util.List;
+import com.sun.jdi.IntegerType;
+
+import java.util.*;
 
 public class StreamMinMaxExample {
     public static void main(String[] args) {
         List<Integer> nums = Arrays.asList(6,7,8,9,10);
         System.out.println(findMaxValue(nums));
+        List<Integer> num = new ArrayList<>();
+        Optional<Integer> opt = findMaxValueOptional(nums);
+        if(opt.isPresent()){
+            System.out.println(opt.get());
+        }
+        else{
+            System.out.println("List is empty");
+        }
     }
 
     public static int findMaxValue(List<Integer> nums){
         return nums.stream()
                 .reduce(0,(acc, cVal) -> cVal > acc ? cVal : acc);
+    }
+
+    public static Optional<Integer> findMaxValueOptional(List<Integer> nums){
+        return nums.stream()
+                .reduce((acc, cVal) -> cVal > acc ? cVal : acc);
     }
 }
