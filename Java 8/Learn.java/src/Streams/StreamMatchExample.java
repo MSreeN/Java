b5b3ed65339dc0e76c8e55ce8157a7db19f8a9cc
MSreeN <khyathimnsree369@gmail.com>
2023-09-12 21:19:15 +0530

    allMatch

--- Java 8/Learn.java/src/Streams/StreamMatchExample.java
+++ Java 8/Learn.java/src/Streams/StreamMatchExample.java
@@ -6,12 +6,16 @@ import java.util.function.Predicate;
 
 public class StreamMatchExample {
     public static void main(String[] args) {
-        Predicate<com.learnJava.data.Student> studentPredicate = student -> student.getGpa()>=4;
-        System.out.println(anyMatch(studentPredicate));
+        Predicate<com.learnJava.data.Student> studentPredicate = student -> student.getGpa()<=4;
+        System.out.println(allMatch(studentPredicate));
     }
 
     public static Boolean anyMatch(Predicate<com.learnJava.data.Student> predicate){
 
         return StudentDataBase.getAllStudents().stream().anyMatch(predicate);
+    }
+
+    public static Boolean allMatch(Predicate<com.learnJava.data.Student> predicate){
+        return StudentDataBase.getAllStudents().stream().allMatch(predicate);
     }
 }
