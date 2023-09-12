cf74df3ccd5f77965d85d74ce9d5f574a9bb4571
MSreeN <khyathimnsree369@gmail.com>
2023-09-12 21:20:24 +0530

    noneMatch

--- Java 8/Learn.java/src/Streams/StreamMatchExample.java
+++ Java 8/Learn.java/src/Streams/StreamMatchExample.java
@@ -6,8 +6,8 @@ import java.util.function.Predicate;
 
 public class StreamMatchExample {
     public static void main(String[] args) {
-        Predicate<com.learnJava.data.Student> studentPredicate = student -> student.getGpa()<=4;
-        System.out.println(allMatch(studentPredicate));
+        Predicate<com.learnJava.data.Student> studentPredicate = student -> student.getGpa()>=4;
+        System.out.println(noneMatch(studentPredicate));
     }
 
     public static Boolean anyMatch(Predicate<com.learnJava.data.Student> predicate){
@@ -17,5 +17,9 @@ public class StreamMatchExample {
 
     public static Boolean allMatch(Predicate<com.learnJava.data.Student> predicate){
         return StudentDataBase.getAllStudents().stream().allMatch(predicate);
+    }
+
+    public static Boolean noneMatch(Predicate<com.learnJava.data.Student> predicate){
+        return StudentDataBase.getAllStudents().stream().noneMatch(predicate);
     }
 }
