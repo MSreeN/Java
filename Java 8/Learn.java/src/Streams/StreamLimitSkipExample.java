bdaa8946583661238db8a68b83eefc4e9c9dd0c7
MSreeN <khyathimnsree369@gmail.com>
2023-09-12 21:03:31 +0530

    limit and skip with reduce

--- Java 8/Learn.java/src/Streams/StreamLimitSkipExample.java
+++ Java 8/Learn.java/src/Streams/StreamLimitSkipExample.java
@@ -1,22 +1,30 @@
 package Streams;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class StreamLimitSkipExample {
     public static void main(String[] args) {
         List<Integer> nums = Arrays.asList(5, 6, 7, 8, 9, 10);
-        System.out.println(skip(nums));
+        Optional<Integer> limit = skip(nums);
+        if(limit.isPresent()){
+            System.out.println(limit.get());
+        }
+        else System.out.println("list is empty");
     }
 
-    public static List<Integer> limit(List<Integer> nums){
-        return nums.stream().limit(5).collect(Collectors.toList());
+    public static Optional<Integer> limit(List<Integer> nums){
+        return nums.stream().limit(5)
+                .reduce((acc, cVal) -> acc+cVal);
+
     }
 
-    public static List<Integer> skip(List<Integer> nums){
-        return nums.stream().skip(3).collect(Collectors.toList());
+    public static Optional<Integer> skip(List<Integer> nums){
+        return nums.stream().skip(3).reduce((acc,cVal) -> acc+cVal);
     }
 
 
