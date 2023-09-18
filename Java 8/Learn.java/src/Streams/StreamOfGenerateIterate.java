71367042eec0682f3bd3abf856fe012150bf0dde
MSreeN <khyathimnsree369@gmail.com>
2023-09-18 22:35:17 +0530

    Stream generate method

--- Java 8/Learn.java/src/Streams/StreamOfGenerateIterate.java
+++ Java 8/Learn.java/src/Streams/StreamOfGenerateIterate.java
@@ -1,11 +1,13 @@
 package Streams;
 
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class StreamOfGenerateIterate {
     public static void main(String[] args) {
 //        ofMethod().forEach( System.out::println);
-        iterateMethod().limit(10).forEach(System.out::println);
+//        iterateMethod().limit(10).forEach(System.out::println);
+        generate().limit(10).forEach(System.out::println);
     }
 
     public static Stream ofMethod(){
@@ -15,4 +17,15 @@ public class StreamOfGenerateIterate {
     public static Stream iterateMethod(){
         return Stream.iterate( 1, x->x*2);
     }
+
+    public static Stream generate(){
+        return Stream.generate(new Supplier<Object>() {
+
+            @Override
+            public Object get() {
+                return Math.ceil(Math.random()* 5);
+            }
+        });
+    }
+
 }
