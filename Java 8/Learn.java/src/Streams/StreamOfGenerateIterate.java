ef8dac717aacf12f0e7f5dabf39a4d4ff7c5896e
MSreeN <khyathimnsree369@gmail.com>
2023-09-18 22:29:20 +0530

    Stream iterate method

--- Java 8/Learn.java/src/Streams/StreamOfGenerateIterate.java
+++ Java 8/Learn.java/src/Streams/StreamOfGenerateIterate.java
@@ -4,12 +4,15 @@ import java.util.stream.Stream;
 
 public class StreamOfGenerateIterate {
     public static void main(String[] args) {
-        ofMethod().forEach( System.out::println);
+//        ofMethod().forEach( System.out::println);
+        iterateMethod().limit(10).forEach(System.out::println);
     }
 
     public static Stream ofMethod(){
         return Stream.of("hello", "world");
     }
 
-
+    public static Stream iterateMethod(){
+        return Stream.iterate( 1, x->x*2);
+    }
 }
